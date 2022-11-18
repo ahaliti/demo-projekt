976e62756af1099b8dd14783b4b1177cd6e71e81
Ardi Haliti <103785655+ahaliti@users.noreply.github.com>
2022-11-18 08:41:08 +0100

    Revert "First dependencies"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,17 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-projekt';
-  something = '';
-  things = ['Home', 'car', 'Try Out'];
-
-  setName(event: Event) {
-    this.something = (event.target as HTMLInputElement).value;
-  }
-
-  click() {
-    console.log(this.something)
-    this.things.push.apply(this.something);
-    this.things.push("hello");
-  }
-
 }
